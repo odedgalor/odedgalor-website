7aa8160729132d215553e167ba4cc52ac262ea7c
Oded Galor <oded_galor@brown.edu>
2026-05-25 09:58:59 +0300

    Intelligence²: embed YouTube player starting at 2:37; support start time in video.js

--- js/video.js
+++ js/video.js
@@ -1,7 +1,8 @@
 document.querySelectorAll('.yt-container').forEach(function(el) {
   el.addEventListener('click', function() {
     var id = el.dataset.id;
-    el.innerHTML = '<iframe src="https://www.youtube.com/embed/' + id + '?autoplay=1" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>';
+    var start = el.dataset.start ? '&start=' + el.dataset.start : '';
+    el.innerHTML = '<iframe src="https://www.youtube.com/embed/' + id + '?autoplay=1' + start + '" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>';
     el.classList.add('yt-playing');
   });
 });
